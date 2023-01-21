0f0adf1c44f4a3d6ceda15e814ea2453ad3d5791
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-01-21 17:42:04 -0300

    feat: Aula 03 - Infraestrutura e injeção de dependências

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { NotificationsRepository } from 'src/app/repositories/notification-repository';
+import { NotificationsRepository } from '@app/repositories/notification-repository';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
 
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { SendNotification } from 'src/app/use-cases/send-notification';
+import { SendNotification } from '@app/use-cases/send-notification';
 import { DatabaseModel } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
 
 @Module({
     imports: [DatabaseModel],
     controllers: [NotificationsController],
-    providers: [SendNotification],
+    providers: [
+        SendNotification,
+        CancelNotification,
+        CountRecipientNotifications,
+        GetRecipientNotifications,
+        ReadNotification,
+        UnreadNotification,
+    ],
 })
 export class HttpModule {}
